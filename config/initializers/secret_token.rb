4ec576833ab9f9f306b8ca62e71baa39fde8d63b
James Thullbery <ifndef@gmail.com>
2014-03-25 11:54:07 -0700

    test for parameters being set to nil

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestParams::Application.config.secret_key_base = '035806a4b36b897bcbdc6264594bd3b72c8a867ecdb15c5351c454af7a7f906b43cbe3fb91c168a428a0018823c4514f3a2320b1a020a5d9d09b6556784fdbf4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestParams::Application.config.session_store :cookie_store, key: '_test_params_session'
